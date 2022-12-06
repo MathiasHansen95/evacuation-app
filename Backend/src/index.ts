b0a3b2adf84fd1bac896c6909f2717ebdb423556
MathiasHansen95 <maha520@student.sdu.dk>
2022-12-06 13:26:46 +0100

    added everything

---
+++ Backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+require('dotenv').config();
+
+export const app = express()
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+let corsOptions;
+
+if (process.env.NODE_ENV === 'development') {
+  corsOptions = {
+    origin: "*"
+  }
+} else {
+  corsOptions = {
+    origin: "https://iob.news"
+  }
+} 
+
+app.use(cors(corsOptions));
